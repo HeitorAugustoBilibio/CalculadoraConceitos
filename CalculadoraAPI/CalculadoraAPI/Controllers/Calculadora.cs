4c73d8b182e5c1a6cbc75cdebaa02d89085c38ae
Heitor <heitoraugustobilibio4@gmail.com>
2025-07-10 23:18:13 -0300

    Primeiro Commit

---
+++ CalculadoraAPI/CalculadoraAPI/Controllers/Calculadora.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Calculadora : Controller
+    {
+        [HttpPost]
+        [Route("Soma")]
+        public IActionResult Soma([FromBody] decimal numeros1, decimal numero2)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Subtracao")]
+        public IActionResult Subtracao([FromBody] decimal numeros1, decimal numero2)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Divisão")]
+        public IActionResult Divisão([FromBody] decimal numeros1, decimal numero2)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Multiplicação")]
+        public IActionResult Multiplicação([FromBody] decimal numeros1, decimal numero2)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("RaizQuadrada")]
+        public IActionResult RaizQuadrada([FromBody] decimal numeros1, decimal numero2)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Potenciação")]
+        public IActionResult Potenciação([FromBody] decimal numeros1, decimal numero2)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ CalculadoraAPI/Domain/Operacoes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Operacoes
+    {
+        public string Soma { get; set; }
+        public string Subtração { get; set; }
+        public string Divisão { get; set; }
+        public string Multiplicação { get; set; }
+        public string RaizQuadrada { get; set; }
+        public string Potenciação { get; set; }
+    }
+}
